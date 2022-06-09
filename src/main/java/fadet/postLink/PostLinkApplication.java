d211493b0616dc1f2acd6da901de4b8cf6ec709e
kth <rnjsxo1017@gmail.com>
2022-06-09 16:09:31 +0900

    시작

---
+++ src/main/java/fadet/postLink/PostLinkApplication.java
@@ -0,0 +1,13 @@
+package fadet.postLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fadet/postLink/PostLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package fadet.postLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
